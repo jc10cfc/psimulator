abfb2541379e8a484eb74bb6dd0b53156a4c2235
haldyr@gmail.com <haldyr@gmail.com@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2011-05-03 11:43:36 +0000

    obrazky pro wiki+update start_server.sh

--- bp_psi/src/spousteni/start_server.sh
+++ bp_psi/src/spousteni/start_server.sh
@@ -9,6 +9,12 @@ help() {
     echo "For reading only skelet of computers and interfaces (without settings) use optional arg '-n'"
 }
 
+if [ ! -e psimulator.dtd ]; then
+    echo "File with DTD (psimulator.dtd) must be in this directory: $PWD"
+    echo "Exiting.."
+    exit 2
+fi
+
 if [ -z "$1" -o "$1" == "-h" -o "$1" == "--help" ]; then
     help
     exit 1
