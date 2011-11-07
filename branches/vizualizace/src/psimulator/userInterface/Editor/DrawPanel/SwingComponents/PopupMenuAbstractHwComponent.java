b8b267d2f4908f330215c6a1855723815c56b21e
svihlik.m@gmail.com <svihlik.m@gmail.com@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2011-11-07 20:27:58 +0000

    bug fixes
    FitToSize and Align to grid needs better icons

--- branches/vizualizace/src/psimulator/userInterface/Editor/DrawPanel/SwingComponents/PopupMenuAbstractHwComponent.java
+++ branches/vizualizace/src/psimulator/userInterface/Editor/DrawPanel/SwingComponents/PopupMenuAbstractHwComponent.java
@@ -14,50 +14,60 @@ import psimulator.userInterface.Editor.DrawPanel.Enums.DrawPanelAction;
  *
  * @author Martin
  */
-public class PopupMenuAbstractHwComponent extends JPopupMenu{
-    
+public class PopupMenuAbstractHwComponent extends JPopupMenu {
+
     private DataLayerFacade dataLayer;
     private DrawPanelInnerInterface drawPanel;
-    
     private JMenuItem jItemAlignToGrid;
     private JMenuItem jItemComponentProperties;
     private JMenuItem jItemSelectAll;
     private JMenuItem jItemDeleteComponent;
-    
-    public PopupMenuAbstractHwComponent(DrawPanelInnerInterface drawPanel, DataLayerFacade dataLayer, int components){
+    private JMenuItem jItemFitToSize;
+
+    public PopupMenuAbstractHwComponent(DrawPanelInnerInterface drawPanel, DataLayerFacade dataLayer, int components) {
         this.dataLayer = dataLayer;
         this.drawPanel = drawPanel;
-        
+
         jItemComponentProperties = new JMenuItem(dataLayer.getString("PROPERTIES"));
         jItemAlignToGrid = new JMenuItem(dataLayer.getString("ALIGN_TO_GRID"));
         jItemDeleteComponent = new JMenuItem(dataLayer.getString("DELETE"));
         jItemSelectAll = new JMenuItem(dataLayer.getString("SELECT_ALL"));
-        
+        jItemFitToSize = new JMenuItem(dataLayer.getString("FIT_TO_SIZE"));
+
         jItemAlignToGrid.addActionListener(drawPanel.getAbstractAction(DrawPanelAction.ALIGN_COMPONENTS_TO_GRID));
         jItemDeleteComponent.addActionListener(drawPanel.getAbstractAction(DrawPanelAction.DELETE));
         jItemSelectAll.addActionListener(drawPanel.getAbstractAction(DrawPanelAction.SELECT_ALL));
-        
-        
-        // add buttons for operations on one component
-        if(components == 1){
-            this.add(jItemComponentProperties);
-            this.addSeparator();
-        }
-        
-        this.add(jItemAlignToGrid);
-        
-        // if 0 components marked, do not add these buttons
-        if(components != 0){
-            this.add(jItemDeleteComponent);
-        }else{
-            this.add(jItemSelectAll);
-        }
-        
-       
-    }
-    
+        jItemFitToSize.addActionListener(drawPanel.getAbstractAction(DrawPanelAction.FIT_TO_SIZE));
 
-    public void show(DrawPanelInnerInterface drawPanel, int x, int y){
-        super.show((JComponent)drawPanel, x, y);
+        if (components == 1) {
+            createOneComponentMenu();
+        } else if (components == 0) {
+            createAllComponentsMenu();
+        } else {
+            createMoreComponentsMenu();
+        }
+
+    }
+
+    private void createOneComponentMenu() {
+        this.add(jItemComponentProperties);
+        this.addSeparator();
+        this.add(jItemAlignToGrid);
+        this.add(jItemDeleteComponent);
+    }
+
+    private void createMoreComponentsMenu() {
+        this.add(jItemAlignToGrid);
+        this.add(jItemDeleteComponent);
+    }
+
+    private void createAllComponentsMenu() {
+        this.add(jItemAlignToGrid);
+        this.add(jItemSelectAll);
+        this.add(jItemFitToSize);
+    }
+
+    public void show(DrawPanelInnerInterface drawPanel, int x, int y) {
+        super.show((JComponent) drawPanel, x, y);
     }
 }
