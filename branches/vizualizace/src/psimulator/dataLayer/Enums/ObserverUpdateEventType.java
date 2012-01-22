bfaf123bb49036d8663f6b9959c406c5071f9f41
svihlik.m@gmail.com <svihlik.m@gmail.com@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-01-22 14:42:14 +0000

    autoscroll in table when playing and moving around

--- branches/vizualizace/src/psimulator/dataLayer/Enums/ObserverUpdateEventType.java
+++ branches/vizualizace/src/psimulator/dataLayer/Enums/ObserverUpdateEventType.java
@@ -5,16 +5,16 @@ package psimulator.dataLayer.Enums;
  * @author Martin
  */
 public enum ObserverUpdateEventType {
-    LANGUAGE,
-    ICON_SIZE,
-    SIMULATOR,
-    SIMULATOR_PLAYER_LIST_MOVE,
-    SIMULATOR_PLAYER_PLAY,
-    SIMULATOR_PLAYER_STOP,
-    //SIMULATOR_PLAYER_START,
-    SIMULATOR_PLAYER_NEXT,
-    SIMULATOR_SPEED,
-    SIMULATOR_DETAILS,
-    SIMULATOR_RECORDER,
-    SIMULATOR_CONNECTION;
+    LANGUAGE,       // when language changed
+    ICON_SIZE,      // when icon size changed
+    
+    SIMULATOR_PLAYER_LIST_MOVE, // when manually changed current event(NEXT, PREV, FIRST, LAST or DOUBLE CLICK on some event)
+    SIMULATOR_PLAYER_PLAY,      // when playing starts
+    SIMULATOR_PLAYER_STOP,      // when playing stops (maunal, or hitting end of list = automatic)
+    SIMULATOR_PLAYER_NEXT,      // automatic move to next event when playing
+    SIMULATOR_SPEED,            // when speed is changed
+    SIMULATOR_RECORDER,         // when recorder is turned on / off
+    SIMULATOR_CONNECTION,       // when connected / disconnected
+    SIMULATOR_DETAILS;          
+    
 }
