d8b69dc7a4e817d6a18b8d85860d7e9b8e112ec8
svihlik.m@gmail.com <svihlik.m@gmail.com@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-01-30 15:41:08 +0000

    AbstractNetwork

--- branches/vizualizace/src/psimulator/AbstractNetwork/Interface.java
+++ branches/vizualizace/src/psimulator/AbstractNetwork/Interface.java
@@ -13,4 +13,45 @@ public class Interface implements Serializable{
     
     private String ipAddress;
     private String macAddress;
+
+    public Interface(int ID, Device device, String ipAddress, String macAddress) {
+        this.ID = ID;
+        this.device = device;
+        this.ipAddress = ipAddress;
+        this.macAddress = macAddress;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Device getDevice() {
+        return device;
+    }
+
+    public void setDevice(Device device) {
+        this.device = device;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+    
+    
 }
