a7290a4fb7544f0bd5ddac75ac901a5e19c7ef58
svihlik.m@gmail.com <svihlik.m@gmail.com@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-02-02 18:10:43 +0000

    Highlighted selected tool in ToolPopupMenu in tool bar

--- branches/vizualizace/src/psimulator/userInterface/SimulatorEditor/SwingComponents/ToolPopupMenu.java
+++ branches/vizualizace/src/psimulator/userInterface/SimulatorEditor/SwingComponents/ToolPopupMenu.java
@@ -5,9 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.ButtonGroup;
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
+import javax.swing.*;
 import psimulator.dataLayer.DataLayerFacade;
 import psimulator.userInterface.SimulatorEditor.Tools.AbstractCreationTool;
 import psimulator.userInterface.SimulatorEditor.Tools.AbstractTool;
@@ -20,7 +18,7 @@ import psimulator.userInterface.imageFactories.AbstractImageFactory;
 public class ToolPopupMenu extends JPopupMenu{
     
     private List<AbstractTool> tools;
-    private List<JMenuItem> jMenuItems;
+    private List<JRadioButtonMenuItem> jMenuItems;
     private MenuToggleButton toggleButton;
     private ButtonGroup toolsButtonGroup;
     
@@ -30,7 +28,7 @@ public class ToolPopupMenu extends JPopupMenu{
         this.toggleButton = toggleButton;
         this.tools = tools;
      
-        jMenuItems = new ArrayList<JMenuItem>();
+        jMenuItems = new ArrayList<JRadioButtonMenuItem>();
         toolsButtonGroup = new ButtonGroup();
         
         for (final AbstractTool tool : tools) {
@@ -38,7 +36,7 @@ public class ToolPopupMenu extends JPopupMenu{
             AbstractCreationTool tmp = (AbstractCreationTool)tool;
 
             // create menu item for tool
-            JMenuItem mi = new JMenuItem(/*tmp.getName() + tmp.getParameterLabel() + tmp.getParameter(), */
+            JRadioButtonMenuItem mi = new JRadioButtonMenuItem(/*tmp.getName() + tmp.getParameterLabel() + tmp.getParameter(), */
                     tmp.getImageIcon(AbstractImageFactory.ICON_SIZE_MENU_BAR_POPUP));
             
             jMenuItems.add(mi);
@@ -54,7 +52,9 @@ public class ToolPopupMenu extends JPopupMenu{
             this.add(mi);
             toolsButtonGroup.add(mi);
         }
-  
+        
+        // set the first selected
+        jMenuItems.get(0).setSelected(true);
     }
     
     @Override
