1b2191e0bafe2134d2c2621ccea1909a233b3356
marlukcz <marlukcz@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-02-12 18:09:51 +0000

    XML -- load and save method

--- branches/vizualizace/src/psimulator/AbstractNetwork/Network.java
+++ branches/vizualizace/src/psimulator/AbstractNetwork/Network.java
@@ -1,49 +1,85 @@
 package psimulator.AbstractNetwork;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 /**
  *
  * @author Martin
  */
-public class Network implements Serializable{
+public class Network implements Serializable {
     //private int ID;
     //private String name;
-    
+
     private NetworkCounter counter;
-    
     private List<NetworkCable> cables;
     private List<NetworkDevice> devices;
-    
     // needed for Graph restore from Network - fast lookup
     private LinkedHashMap<Integer, NetworkInterface> interfacesMap;
 
-    public Network(/*int ID, String name*/) {
+    public Network(/*
+             * int ID, String name
+             */) {
         //this.ID = ID;
         //this.name = name;
-        
+
         this.cables = new ArrayList<NetworkCable>();
         this.devices = new ArrayList<NetworkDevice>();
-        
+
         this.interfacesMap = new LinkedHashMap<Integer, NetworkInterface>();
     }
-    
-    public void addDevice(NetworkDevice device){
+
+    public void save(String fileName) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance("psimulator.AbstractNetwork");
+
+        File file = new File(fileName);
+
+        Marshaller marsh = context.createMarshaller();
+
+        // nastavení formátování
+        marsh.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        marsh.marshal(this, file);
+
+    }
+
+    public static Network load(String fileName) throws JAXBException {
+
+        File file = new File(fileName);
+
+        Network network = null;
+
+        JAXBContext context = JAXBContext.newInstance("psimulator.AbstractNetwork");
+
+        Unmarshaller unmarsh = context.createUnmarshaller();
+
+        network = (Network) unmarsh.unmarshal(file);
+
+        return network;
+
+
+    }
+
+    public void addDevice(NetworkDevice device) {
         devices.add(device);
     }
-    
-    public void addCable(NetworkCable cable){
+
+    public void addCable(NetworkCable cable) {
         cables.add(cable);
     }
-    
-    public void addNetworkInterface(NetworkInterface networkInterface){
+
+    public void addNetworkInterface(NetworkInterface networkInterface) {
         interfacesMap.put(networkInterface.getID(), networkInterface);
     }
-    
-    public NetworkInterface getNetworkInterface(int id){
+
+    public NetworkInterface getNetworkInterface(int id) {
         return interfacesMap.get(id);
     }
 
@@ -62,33 +98,23 @@ public class Network implements Serializable{
     public void setCounter(NetworkCounter counter) {
         this.counter = counter;
     }
-    
+
     // ---------------------------------------------------------------
     // Martin Svihlik tyto metody nepotrebuje:
     public void setCables(List<NetworkCable> cables) {
         this.cables = cables;
     }
-    
+
     public void setDevices(List<NetworkDevice> devices) {
         this.devices = devices;
     }
-    
     /*
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-    
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    */
-    
+     * public int getID() { return ID; }
+     *
+     * public void setID(int ID) { this.ID = ID; }
+     *
+     * public String getName() { return name; }
+     *
+     * public void setName(String name) { this.name = name; }
+     */
 }
