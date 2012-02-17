20ee164720a848aea4d7b934cd4373cb6fc1a4bb
svihlik.m@gmail.com <svihlik.m@gmail.com@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-02-17 12:49:41 +0000

    New TimingFramework for timing animations. Animation panel refactored.

--- src/psimulator/userInterface/SimulatorEditor/AnimationPanel/AnimationPanel.java
+++ src/psimulator/userInterface/SimulatorEditor/AnimationPanel/AnimationPanel.java
@@ -1,13 +1,16 @@
 package psimulator.userInterface.SimulatorEditor.AnimationPanel;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.util.List;
 import java.util.*;
-import javax.swing.Timer;
+import javax.swing.JComponent;
+import org.jdesktop.core.animation.rendering.JRenderer;
+import org.jdesktop.core.animation.rendering.JRendererTarget;
+import org.jdesktop.core.animation.timing.Animator;
+import org.jdesktop.core.animation.timing.TimingSource;
+import org.jdesktop.swing.animation.rendering.JRendererFactory;
+import org.jdesktop.swing.animation.rendering.JRendererPanel;
+import org.jdesktop.swing.animation.timing.sources.SwingTimerTimingSource;
 import psimulator.dataLayer.DataLayerFacade;
 import psimulator.dataLayer.Enums.ObserverUpdateEventType;
 import psimulator.userInterface.MainWindowInnerInterface;
@@ -22,16 +25,14 @@ import psimulator.userInterface.imageFactories.AbstractImageFactory;
  *
  * @author Martin
  */
-public class AnimationPanel extends AnimationPanelOuterInterface implements ActionListener{
-
+public class AnimationPanel extends AnimationPanelOuterInterface {
+    //
+    private static final TimingSource f_repaintTimer = new SwingTimerTimingSource();
+    //
     private AbstractImageFactory imageFactory;
     private ZoomManager zoomManager;
-    
     private Graph graph;
     //
-    //private Thread animator;
-    private Timer timer;
-    private final int DELAY = 25;
     //
     private List<Animation> animations;
     //
@@ -41,41 +42,39 @@ public class AnimationPanel extends AnimationPanelOuterInterface implements Acti
             AbstractImageFactory imageFactory, DataLayerFacade dataLayer, ZoomManager zoomManager,
             DrawPanelOuterInterface drawPanel) {
 
+        super();
+        Animator.setDefaultTimingSource(f_repaintTimer);
+
         this.imageFactory = imageFactory;
         this.zoomManager = zoomManager;
-        
+
         this.setOpaque(false);
-        
+
         animations = Collections.synchronizedList(new ArrayList<Animation>());
 
-        timer = new Timer(DELAY, this);
-        
-        setDoubleBuffered(true);
 
+        f_repaintTimer.init();
+        f_repaintTimer.addPostTickListener(new TimingSource.PostTickListener() {
+            @Override
+            public void timingSourcePostTick(TimingSource source, long nanoTime) {
+                repaint();
+            }
+        });
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Graphics2D g2d = (Graphics2D) g;
-        
-        Iterator<Animation> it= animations.iterator();
-        
-        while(it.hasNext()){
-            Animation animation = it.next();
-            // convert X and Y to actual using zoom manager
-            g2d.drawImage(animation.getImage(), animation.getX(), animation.getY(), this);
+        Graphics2D g2 = (Graphics2D) g;
+
+        Iterator<Animation> it = animations.iterator();
+        while (it.hasNext()) {
+            Animation animation = it.next(); // convert X and Yto actual using zoom manager 
+            g2.drawImage(animation.getImage(), animation.getX(), animation.getY(), null);
         }
- 
-        g2d.drawRect(0, 0, getWidth(), getHeight());
-        
-        Toolkit.getDefaultToolkit().sync();
-        g.dispose();
     }
-    
-        
-    
+
     @Override
     public void update(Observable o, Object o1) {
         switch ((ObserverUpdateEventType) o1) {
@@ -86,11 +85,21 @@ public class AnimationPanel extends AnimationPanelOuterInterface implements Acti
                 break;
         }
     }
-    
 
+    private void removeAllAnimations(){
+        Iterator<Animation> it = animations.iterator();
+        while (it.hasNext()) {
+            Animation animation = it.next(); // convert X and Yto actual using zoom manager 
+            animation.stopAnimator();
+            it.remove();
+        }
+        
+        animations.clear();
+    }
+    
     @Override
     public Graph removeGraph() {
-        timer.stop();
+        removeAllAnimations();
         
         Graph tmp = graph;
         graph = null;
@@ -100,37 +109,20 @@ public class AnimationPanel extends AnimationPanelOuterInterface implements Acti
     @Override
     public void setGraph(Graph graph) {
         this.graph = graph;
-        this.setBounds(0, 0, graph.getPreferredSize().width-1, graph.getPreferredSize().height-1);
+        this.setBounds(0, 0, graph.getPreferredSize().width - 1, graph.getPreferredSize().height - 1);
 
         List<AbstractHwComponent> list = new ArrayList<AbstractHwComponent>(graph.getHwComponents());
-        
-        for (int i = 0; i < 10; i++) {
-            
+
+        for (int i = 0; i < 1000; i++) {
+
             int componentCount = graph.getAbstractHwComponentsCount();
             int i1 = random.nextInt(componentCount);
             int i2 = random.nextInt(componentCount);
-            
+
             Point p1 = list.get(i1).getCenterLocationDefaultZoom();
             Point p2 = list.get(i2).getCenterLocationDefaultZoom();
-            
-            animations.add(new Animation(imageFactory, zoomManager, p1, p2));          
+
+            animations.add(new Animation(imageFactory, zoomManager, p1, p2));
         }
-        
-        timer.start();
-    }
-
-    public void cycle() {
-        Iterator<Animation> it= animations.iterator();
-        
-        while(it.hasNext()){
-            it.next().move();
-        }
-    }
-
-
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        cycle();
-        repaint();  
     }
 }
