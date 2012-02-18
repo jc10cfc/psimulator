0418b6dda898da3385145122a54027d2d0c75e97
marlukcz <marlukcz@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-02-18 11:32:08 +0000

    additions preparation

---
+++ branches/vizualizace/src/psimulator/AbstractNetwork/xml/SimNetworkDeviceNullAdapter.java
@@ -0,0 +1,25 @@
+
+
+package psimulator.AbstractNetwork.xml;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import psimulator.AbstractNetwork.AdditionsSimulator.SimNetworkDevice;
+
+/**
+ *
+ * @author Martin Lukáš <lukasma1@fit.cvut.cz>
+ */
+public class SimNetworkDeviceNullAdapter extends XmlAdapter<SimNetworkDevice,Integer> {
+
+    @Override
+    public Integer unmarshal(SimNetworkDevice v) throws Exception {
+        return null;
+    }
+
+    @Override
+    public SimNetworkDevice marshal(Integer v) throws Exception {
+        return null;
+    }
+
+
+}
