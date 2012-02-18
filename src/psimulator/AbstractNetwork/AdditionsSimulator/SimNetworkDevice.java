a8ac85d030bba887b3aadf7870067170d72fa08b
marlukcz <marlukcz@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-02-18 11:32:08 +0000

    additions preparation

---
+++ src/psimulator/AbstractNetwork/AdditionsSimulator/SimNetworkDevice.java
@@ -0,0 +1,25 @@
+
+
+package psimulator.AbstractNetwork.AdditionsSimulator;
+
+/**
+ *
+ * @author Martin Lukáš <lukasma1@fit.cvut.cz>
+ */
+public class SimNetworkDevice {
+
+    private String filesystemFolder;
+
+    public SimNetworkDevice() {
+    }
+
+    public String getFilesystemFolder() {
+        return filesystemFolder;
+    }
+
+    public void setFilesystemFolder(String filesystemFolder) {
+        this.filesystemFolder = filesystemFolder;
+    }
+
+    
+}
