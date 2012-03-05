cd15b44fbdcfbdf2cbd589bf6abbd1f2746981c2
marlukcz <marlukcz@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-03-05 15:23:36 +0000

    stuff copied

---
+++ branches/vizualizace/src/psimulator/dataLayer/Network/Components/simulatorConfig/NatConfig.java
@@ -0,0 +1,13 @@
+/*
+ * created 3.3.2012
+ */
+
+package psimulator.dataLayer.Network.Components.simulatorConfig;
+
+/**
+ *
+ * @author Stanislav Rehak <rehaksta@fit.cvut.cz>
+ */
+public class NatConfig {
+
+}
