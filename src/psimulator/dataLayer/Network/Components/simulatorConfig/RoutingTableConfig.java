8ed312b19aa2e3b98cb03e389e902f87acb04df7
marlukcz <marlukcz@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-03-05 10:05:56 +0000

    psimulator backend stuff copied... load and save into xml tested

---
+++ src/psimulator/dataLayer/Network/Components/simulatorConfig/RoutingTableConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Erstellt am 2.3.2012.
+ */
+
+package psimulator.dataLayer.Network.Components.simulatorConfig;
+
+/**
+ *
+ * @author Tomas Pitrinec
+ */
+public class RoutingTableConfig {
+
+    public RoutingTableConfig() {
+    }
+    
+    
+
+}
