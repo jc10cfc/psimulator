47a0ce40167c0ef5fb9a0c56ed6874da4231bb79
marlukcz <marlukcz@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-03-06 10:15:36 +0000

    delete shared stuff from UI

--- src/psimulator/dataLayer/Network/Components/Identifiable.java
+++
@@ -1,9 +0,0 @@
-package psimulator.dataLayer.Network.Components;
-
-/**
- *
- * @author Martin Švihlík <svihlma1 at fit.cvut.cz>
- */
-public interface Identifiable {
-    public Integer getId();
-}
