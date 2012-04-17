96a06b86a6a10904bc8b15974c2d94d9edce2d6b
svihlik.m@gmail.com <svihlik.m@gmail.com@fd320c38-c5a9-f53d-f2c2-bb773761b19c>
2012-04-17 14:00:55 +0000

    Added some Javadoc

--- src/shared/Components/NameInterface.java
+++ src/shared/Components/NameInterface.java
@@ -5,6 +5,14 @@ package shared.Components;
  * @author Martin Švihlík <svihlma1 at fit.cvut.cz>
  */
 public interface NameInterface {
+    /**
+     * Sets name of component
+     * @param name 
+     */
     public void setName(String name);
+    /**
+     * Gets name of component
+     * @return 
+     */
     public String getName();
 }
--- src/shared/SimulatorEvents/Serializer/AbstractSimulatorEventsSaveLoadInterface.java
+++ src/shared/SimulatorEvents/Serializer/AbstractSimulatorEventsSaveLoadInterface.java
@@ -9,6 +9,19 @@ import java.io.File;
  * @author Martin Švihlík <svihlma1 at fit.cvut.cz>
  */
 public interface AbstractSimulatorEventsSaveLoadInterface {
+    /**
+     * Saves simulator events to specified file. If error occours, Exception is thrown.
+     * @param simulatorEvents
+     * @param file File to save events to
+     * @throws SaveLoadException 
+     */
      public void saveEventsToFile(SimulatorEventsWrapper simulatorEvents, File file) throws SaveLoadException;
+     
+     /**
+      * Loads simulator events from specified file. If error occours, Exception is thrown.
+      * @param file File to load events from
+      * @return
+      * @throws SaveLoadException 
+      */
      public SimulatorEventsWrapper loadEventsFromFile(File file) throws SaveLoadException;
 }
